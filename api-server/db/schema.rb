ffe94c35344768460b349e96cd563d463def2491
Nathaniel <nathaniel.seymour@gmail.com>
2015-08-15 23:09:34 +0200

    API server : add model Post

--- api-server/db/schema.rb
+++ api-server/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815202309) do
+ActiveRecord::Schema.define(version: 20150815210715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "first_name"
